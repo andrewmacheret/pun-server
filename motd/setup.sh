a95fa4b108f931d4732d3fb0ba8975dfbeb037fa
Ubuntu <ubuntu@a1.(none)>
2014-08-03 02:30:14 +0000

    fixed comment

--- motd/setup.sh
+++ motd/setup.sh
@@ -4,7 +4,7 @@
 result_file="/etc/update-motd.d/70-pun"
 pun_script="$( pwd )"/pun.py
 
-# replace __PWD__ in pun-motd and put the results in the intended destination
+# replace __PUN_SCRIPT__ in pun-motd and put the results in the intended destination
 awk -v PUN_SCRIPT="${pun_script}" '{gsub("__PUN_SCRIPT__", PUN_SCRIPT); print}' motd/pun-motd > "$result_file" || exit 1
 
 # make sure ownership and permissions are good enough (probably unnecessary)
