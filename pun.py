f24f24d58d741ec8e07cef25b9808e44f56b093f
Andrew Macheret <andrew.macheret@gmail.com>
2016-10-07 20:19:06 +0000

    Use beautiful soup 4

--- pun.py
+++ pun.py
@@ -5,7 +5,8 @@ from pprint import pprint
 import urllib2
 import socket
 import sys
-from BeautifulSoup import BeautifulSoup
+#import re
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 # read the html content of the random pun page into a string
@@ -19,11 +20,13 @@ except socket.timeout, e:
   sys.exit(1)
 
 # create a beautiful soup object out of the raw html (the prettify is probably not necessary)
-soup = BeautifulSoup(html_content)
+soup = BeautifulSoup(html_content, "lxml")
 soup.prettify()
 
 # find and print the pun... it's the text in the element: div#main-content div.dropshadow1
 pun = soup.find('div', {'id': 'main-content'}).find('div', {'class': 'dropshadow1'}).text
+
+pun = pun.strip()
 
 print pun
 
